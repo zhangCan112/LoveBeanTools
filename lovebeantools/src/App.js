13600f30597c0ed44fa26667b00d3fc88f9805d4
zhangcan <zhangcan@xiaoshouyi.com>
2018-01-08 21:20:24 +0800

    小提交

--- lovebeantools/src/App.js
+++ lovebeantools/src/App.js
@@ -39,13 +39,13 @@ class App extends Component {
                         defaultOpenKeys={['sub1']}
                         style={{ height: '100%', borderRight: 0 }}
                     >
-                        <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                        <SubMenu key="sub1" title={<span><Icon type="user" />日期转换</span>}>
                             <Menu.Item key="1">option1</Menu.Item>
                             <Menu.Item key="2">option2</Menu.Item>
                             <Menu.Item key="3">option3</Menu.Item>
                             <Menu.Item key="4">option4</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                        <SubMenu key="sub2" title={<span><Icon type="laptop" />增长率</span>}>
                             <Menu.Item key="5">option5</Menu.Item>
                             <Menu.Item key="6">option6</Menu.Item>
                             <Menu.Item key="7">option7</Menu.Item>
